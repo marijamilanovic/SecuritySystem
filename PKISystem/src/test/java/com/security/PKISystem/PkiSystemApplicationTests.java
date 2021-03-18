a9ef2de34ce6cdbb1dca311f2aba27f98857abf3
Gojko <gojko.novcic2@gmail.com>
2021-03-18 19:17:09 +0100

    [Init] Project init

---
+++ PKISystem/src/test/java/com/security/PKISystem/PkiSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.PKISystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkiSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
