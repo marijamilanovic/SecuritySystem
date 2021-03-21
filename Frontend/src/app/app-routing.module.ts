b09fd1ffeb8db8f13a5ba28aee3321501403d318
tasa1998 <tasa1998@uns.ac.rs>
2021-03-22 00:33:46 +0100

    [Frontend] add certificate

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddCertificateComponent } from './components/add-certificate/add-certificate.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/addCertificate', pathMatch: 'full' },
-  { path: 'addCertificate', component: AddCertificateComponent }
+  { path: 'addCertificate', component: AddCertificateComponent },
+  { path: 'nav', component: NavbarComponent }
 ];
 
 @NgModule({
