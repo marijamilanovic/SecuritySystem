bfa6f78513788a6c491f920706f025bb6bc12efe
marijamilanovic <marijamil@uns.ac.rs>
2021-03-22 22:37:29 +0100

    [Frontend] modified page for adding new Certificate

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AddCertificateComponent } from './components/add-certificate/add-certificate.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ValidationCertificateComponent } from './components/validation-certificate/validation-certificate.component';
@@ -22,6 +24,7 @@ import { AllCertificatesComponent } from './components/all-certificates/all-cert
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
